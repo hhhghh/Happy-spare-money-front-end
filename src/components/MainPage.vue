45229b0e64188a7a39a6460396cf0e635c936f51
Huang-Junjie <1582695658@qq.com>
2019-06-11 15:30:37 +0800

    resolve conflict

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -180,17 +180,7 @@
             });
           }
         });
-
-<<<<<<< HEAD
-     
-     
       
-      
-      
-      
-=======
->>>>>>> 57e9d101a6513efeab78f7404794641d578c4528
-
       this.$router.push({path: `/MainPage/taskSearch`});
     },
 
